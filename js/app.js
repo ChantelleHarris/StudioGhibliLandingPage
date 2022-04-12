9a5af743f0ee2e735a8d08e8779e278711bde1e9
Chantelle Willow Spirit <chantellewillowspirit@Chantelles-MBP.attlocal.net>
2022-04-11 23:29:28 -0400

    I think it'll pass the assignment, when it does, I'll come back to finish the content.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ let sectionTitles = document.getElementsByTagName('section');
 //This changes the HTML Collection into an array
 let sectionTitlesArray = Array.from(sectionTitles);
 
+
 //This selects the last section of the sectionTitlesArray and saves it to a variable
 let lastItem = sectionTitlesArray.slice(-1);
 
@@ -29,6 +30,7 @@ function appendLastItem () {
     //This adds styling to the navigation's last child by setting classes and an id
     navigationBar.lastChild.children[0].classList.add('title-animation');
     navigationBar.lastChild.children[0].classList.add('connect');
+    navigationBar.lastChild.children[0].classList.add('nav-anchors');
     navigationBar.lastChild.children[0].id = 'lastATag';
 
     //This generates a random id
@@ -40,10 +42,9 @@ function appendLastItem () {
     //This grabs that id and sets it as the target of the navigation bar's last child's 'a' tag
     navigationBar.lastChild.children[0].setAttribute("href",`#${randomId}`);   
 
-    //This gives each the footer the newly generated id
+    //This gives the footer the newly generated id
     lastItem[0].id = randomId;
 }
-
 
 
 //This loops through the sectionTitlesArray, except for the last child, to create the navigation bar
@@ -62,8 +63,9 @@ for(let i = 0; i < sectionTitlesArray.length - 1; i++) {
     //This sets the values of the links equal to the titles of each section
     navigationBar.children[i].children[0].innerText = sectionTitles[i].children[0].children[0].innerText;
 
-    //This sets an id to each link
+    //This adds classes to each link
     navigationBar.children[i].children[0].classList.add('nav-links');
+    navigationBar.children[i].children[0].classList.add('nav-anchors');
 
     //This generates a random number-letter combination
     let generateId = Math.random().toString(36).slice(2);
@@ -86,106 +88,66 @@ appendLastItem();
 
 
 
-//Listens for viewport scroll for Introduction
-window.addEventListener('scroll', 
-function addClass() {
-    let isInViewport = false;
+//Selects the anchors of the navigation bar by class and converts the given HTML Collection to an array
+let aTags = document.getElementsByClassName('nav-anchors');
+let aTagsArray = Array.from(aTags);
 
-    //selects element with data-nav attribute and saves it to a variable
-    let intro = document.querySelector('[data-nav="Introduction"]');
 
-    //Gets the viewport bounds and sets that object to a variable
-    let introInViewport = intro.getBoundingClientRect();
+//Sets smooth scrolling behavior for each anchor click
+for(let i = 0; i < aTagsArray.length; i++) {
+    aTagsArray[i].addEventListener('click', function scrollSmooth(e) {
+        e.preventDefault();
+        let sectionsTop = sectionTitlesArray[i].getBoundingClientRect().top;
+        window.scroll({top: `${sectionsTop}`, behavior: "smooth"})
+    })
+}
 
-    //if the element is in the viewport, set class to active, otherwise remove that class
-    if (introInViewport.top >= 0 && introInViewport.left >= 0 && introInViewport.right <= window.innerWidth && introInViewport.bottom <= window.innerHeight) {
-        isInViewport = true;
-        navigationBar.children[0].children[0].classList.add('active');
-    }   else {
-            navigationBar.children[0].children[0].classList.remove('active');
-    } 
-});
+//Adds active class when section is in viewport for all but the Connect section
+window.addEventListener('scroll', function addClass() {
+    for(let i = 0; i < sectionTitlesArray.length - 1; i ++) {
+        let boundary = sectionTitlesArray[i].getBoundingClientRect();
+        if (boundary.top >= 0 && boundary.left >= 0 && boundary.right <= window.innerWidth && boundary.bottom <= window.innerHeight) {
+            navigationBar.children[i].children[0].classList.add('active');
+        } else {
+            navigationBar.children[i].children[0].classList.remove('active');
+        }
+    }
+})
 
-//Listens for viewport scroll for Howl's Moving Castle
-window.addEventListener('scroll', 
-function addClass() {
-    let isInViewport = false;
-    let howl = document.querySelector('[data-nav="Howl"]');
-    let howlInViewport = howl.getBoundingClientRect();
-    if (howlInViewport.top >= 0 && howlInViewport.left >= 0 && howlInViewport.right <= window.innerWidth && howlInViewport.bottom <= window.innerHeight) {
-        isInViewport = true;
-        navigationBar.children[1].children[0].classList.add('active');
-    }   else {
-            navigationBar.children[1].children[0].classList.remove('active');
-    } 
-});
+//Adds active class when section is in viewport for all but the Connect section:MEDIUM SCREENS
+if (window.matchMedia("(min-width: 851px) and (max-width: 999px)").matches) {
+    for(let i = 0; i < sectionTitlesArray.length - 1; i ++) {
+        let boundary = sectionTitlesArray[i].getBoundingClientRect();
+        if (boundary.top >= 0 && boundary.left >= 0 && boundary.right <= window.innerWidth && boundary.bottom <= window.innerHeight) {
+            navigationBar.children[i].children[0].classList.add('active');
+        } else {
+            navigationBar.children[i].children[0].classList.remove('active');
+        }
+    }
+}
 
-//Listens for viewport scroll for Spirited Away
-window.addEventListener('scroll', 
-function addClass() {
-    let isInViewport = false;
-    let spirited = document.querySelector('[data-nav="Spirited"]');
-    let spiritedInViewport = spirited.getBoundingClientRect();
-    if (spiritedInViewport.top >= 0 && spiritedInViewport.left >= 0 && spiritedInViewport.right <= window.innerWidth && spiritedInViewport.bottom <= window.innerHeight) {
-        isInViewport = true;
-        navigationBar.children[2].children[0].classList.add('active');
-    }   else {
-            navigationBar.children[2].children[0].classList.remove('active');
-    } 
-});
+//Adds active class when section is in viewport for all but the Connect section:SMALL SCREENS
+if (window.matchMedia("(min-width: 300px) and (max-width: 850px)").matches) {
+    for(let i = 0; i < sectionTitlesArray.length - 1; i ++) {
+        let boundary = sectionTitlesArray[i].getBoundingClientRect();
+        if (boundary.top >= 0 && boundary.left >= 0 && boundary.right <= window.innerWidth && boundary.bottom <= window.innerHeight) {
+            navigationBar.children[i].children[0].classList.add('active');
+        } else {
+            navigationBar.children[i].children[0].classList.remove('active');
+        }
+    }
+}
 
-//Listens for viewport scroll for Whisper of the Heart
-window.addEventListener('scroll', 
-function addClass() {
-    let isInViewport = false;
-    let whisper = document.querySelector('[data-nav="Whisper"]');
-    let whisperInViewport = whisper.getBoundingClientRect();
-    if (whisperInViewport.top >= 0 && whisperInViewport.left >= 0 && whisperInViewport.right <= window.innerWidth && whisperInViewport.bottom <= window.innerHeight) {
-        isInViewport = true;
-        navigationBar.children[3].children[0].classList.add('active');
-    }   else {
-            navigationBar.children[3].children[0].classList.remove('active');
-    } 
-});
-
-//Listens for viewport scroll for When Marnie Was Here
-window.addEventListener('scroll', 
-function addClass() {
-    let isInViewport = false;
-    let marnie = document.querySelector('[data-nav="Marnie"]');
-    let marnieInViewport = marnie.getBoundingClientRect();
-    if (marnieInViewport.top >= 0 && marnieInViewport.left >= 0 && marnieInViewport.right <= window.innerWidth && marnieInViewport.bottom <= window.innerHeight) {
-        isInViewport = true;
-        navigationBar.children[4].children[0].classList.add('active');
-    }   else {
-            navigationBar.children[4].children[0].classList.remove('active');
-    } 
-});
-
-//Listens for viewport scroll for Connect section
-window.addEventListener('scroll', 
-function addClass() {
-    let isInViewport = false;
+//Adds active class for Connect section(it has different styling)
+window.addEventListener('scroll', function addClass() {
     let connect = document.querySelector('[data-nav="Connect"]');
     let connectInViewport = connect.getBoundingClientRect();
     if (connectInViewport.top >= 0 && connectInViewport.left >= 0 && connectInViewport.right <= window.innerWidth && connectInViewport.bottom <= window.innerHeight) {
-        isInViewport = true;
         navigationBar.lastChild.children[0].classList.add('connect-active');
     }   else {
             navigationBar.lastChild.children[0].classList.remove('connect-active');
     } 
 });
-
-
-// window.addEventListener('scroll', function addClass() {
-//     for(let i = 0; i < sectionTitlesArray.length; i++) {
-
-//     }
-// })
-
-// let aTags = document.getElementsByTagName('a');
-// let aTagsArray = Array.from(aTags);
-// console.log(aTagsArray)
 
 //This makes the quote button content appear and collapse
 //I didn't like the idea of collapsible main sections
@@ -194,6 +156,24 @@ let clicked;
 for(let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function showQuotes() {
         let quotes = document.getElementsByClassName('quote-content');
-            quotes[i].classList.toggle('show-quote');  
+        quotes[i].classList.toggle('show-quote');  
     })
 }
+
+console.log(sectionTitlesArray[0].getBoundingClientRect());
+console.log("Width is " + window.innerWidth);
+console.log("Height is " + window.innerHeight);
+
+//This specifically targets section 1, Introduction, because its too long to meet all of the viewport
+//conditions--so, I have to modify the conditions for it to register responsively
+
+// if (window.matchMedia("(min-width: 300px) and (max-width: 850px)").matches) {
+//     let section1 = sectionTitlesArray[0];
+//     let section1Boundary = section1.getBoundingClientRect();
+//         if (section1Boundary.top >= 0 && section1Boundary.left >= 0 && section1Boundary.right <= window.innerWidth) {
+//             section1.classList.add('active');
+//         } else {
+//             section1.classList.remove('active');
+//         }
+    
+// }
